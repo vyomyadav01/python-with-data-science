cc15f2d76cd8852ac326bd656b3ae2ae3c0ef058
vyomyadav01 <vyomyadav2002@gmail.com>
2022-10-21 18:25:28 +0530

    implementation of loops

--- DA and HRA.py
+++ DA and HRA.py
@@ -1,5 +1,5 @@
 basic_salary = float(input("Enter your basic salary:\n"))
-if basic_salary < 10000:
+if basic_salary <= 10000:
     hra = 0.1 * basic_salary
     da = 0.9 * basic_salary
     print("Your hra according to your basic salary is:\n",hra)
@@ -7,10 +7,10 @@ if basic_salary < 10000:
 elif basic_salary <=20000:
     hra = 0.2 * basic_salary
     da = 0.8 * basic_salary
-    print("Your hra according to your basic salary is:\n",hra)
-    print("Your da according to your basic salary is:\n",da)
+    print(" Your hra according to your basic salary is:\n",hra)
+    print(" Your da according to your basic salary is:\n",da)
 elif basic_salary > 20000:
     hra = 0.25 * basic_salary
     da = 0.75 * basic_salary
     print("Your hra according to your basic salary is:\n",hra)
-    print("Your basic da according to your basic salary is:\n",da)
+    print(" Your basic da according to your basic salary is:\n",da)
