b98dc0d1dec9599c22866eac81142669ea5ffdd9
vyomyadav01 <vyomyadav2002@gmail.com>
2022-10-28 18:29:44 +0530

    for loops and types of loops

--- SI.py
+++ SI.py
@@ -1,4 +1,10 @@
-p = float(input("Enter the principle amount:\n"))
-r = float(input("Enter the rate:\n"))
-t = int(input("Enter the time period:\n"))
-a = print(p*(1+r*t)) #a is the amount
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
